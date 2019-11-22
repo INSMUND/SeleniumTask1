bd7dc7760de1c55aa4e6f8b87fc8ee332397d195
isilonosova <Endavamc141981!>
2019-11-22 10:43:36 +0200

    Added loginPage scenario

---
+++ src/test/java/FirstTest/TestLoginPage.java
@@ -0,0 +1,23 @@
+package FirstTest;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestLoginPage extends TestBase {
+
+	@Test
+	public void loginPageTest() {
+
+		driver.get(configFileReader.getApplicationUrl());
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.name("username")).sendKeys("admin");
+		driver.findElement(By.name("password")).sendKeys("admin");
+		driver.findElement(By.name("login")).click();
+		System.out.println("Title of opened page is:"+driver.getTitle());
+		Assert.assertEquals(driver.getTitle(), "My Store");
+	}
+}
+
